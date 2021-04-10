4c29b917ebe2f3568c8d99376d2363f7c52336ae
KneeDeepInCode <82250600+KneeDeepInCode@users.noreply.github.com>
2021-04-09 23:43:36 -0400

    (very) initial code

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#define DGAF_USE_SDL2
+
+#if defined(DGAF_USE_SDL2)
+//#include <SDL2/SDL.h>
+#elif defined(DGAF_USE_SDL1)
+//#include <SDL/SDL.h>
+#endif
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	std::vector<std::string> args;
+	for (int i = 0; i < argc; i++)
+	{
+		std::string arg;
+		arg.append(argv[i]);
+		args.push_back(arg);
+	}
+	std::cout << args[args.size() - 1] << std::endl;
+	return 0;
+}
